32c256ea6d3233f2f72a943219e84d3c413520da
kuiren su <kuirensu@usc.edu>
2017-10-18 14:48:02 -0700

    newView by Maya

---
+++ TaskScheduler/SettingsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SettingsViewController.swift
+//  TaskScheduler
+//
+//  Created by Kuiren Su on 10/18/17.
+//  Copyright © 2017 Nikhil Cherukuri. All rights reserved.
+//
+
+import Foundation
