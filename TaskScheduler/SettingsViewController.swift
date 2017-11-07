c38f424b4dc9faf3f3a0997ea315f072ef8ae344
Nikhil Cherukuri <nikhilcherukuri@Nikhils-MacBook-Pro.local>
2017-11-06 16:44:31 -0800

    made settings viewcontroller

--- TaskScheduler/SettingsViewController.swift
+++ TaskScheduler/SettingsViewController.swift
@@ -2,8 +2,46 @@
 //  SettingsViewController.swift
 //  TaskScheduler
 //
-//  Created by Kuiren Su on 10/18/17.
+//  Created by Nikhil Cherukuri on 11/6/17.
 //  Copyright © 2017 Nikhil Cherukuri. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class SettingsViewController: UIViewController {
+    @IBOutlet weak var bedTimePicker: UIDatePicker!
+    @IBOutlet weak var startTimePicker: UIDatePicker!
+    
+    @IBOutlet weak var lunchTimePicker: UIDatePicker!
+    
+    @IBOutlet weak var dinnerTimePicker: UIDatePicker!
+    
+    @IBOutlet weak var lunchHoursPicker: UIPickerView!
+    
+    @IBOutlet weak var lunchMinsPicker: UIPickerView!
+    
+    @IBOutlet weak var focusTimePicker: UIDatePicker!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
