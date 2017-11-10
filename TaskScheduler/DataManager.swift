6bd4ae8810602115260bcd92ea0bb0728b9abc8e
Nikhil Cherukuri <nikhilcherukuri@Nikhils-MacBook-Pro.local>
2017-11-10 09:18:59 -0800

    adding settings

---
+++ TaskScheduler/DataManager.swift
@@ -0,0 +1,204 @@
+//
+//  DataManager.swift
+//  TaskScheduler
+//
+//  Created by Nikhil Cherukuri on 11/7/17.
+//  Copyright © 2017 Nikhil Cherukuri. All rights reserved.
+//
+
+import Foundation
+import os.log
+
+class DataManager {
+    struct PropertyKey {
+        static let lunchTimeKey = "lunchTimeKey"
+        static let dinnerTimeKey = "dinnerTimeKey"
+        static let focusTimeKey = "focusTimeKey"
+        static let eatingTimeKey = "eatingTimeKey"
+        static let bedTimeKey = "bedTimeKey"
+        static let startTimeKey = "startTimeKey"
+    }
+   
+    static var sharedInstance = DataManager()
+    
+    static let keychainItemIdentifier = "edu.usc.TaskScheduler"
+    static let dataManagerKey = "dataManagerKey"
+    static let lunchTimeKey = "lunchTimeKey"
+    static let dinnerTimeKey = "dinnerTimeKey"
+    static let focusTimeKey = "focusTimeKey"
+    static let eatingTimeKey = "eatingTimeKey"
+    static let bedTimeKey = "bedTimeKey"
+    static let startTimeKey = "startTimeKey"
+    
+    var lunchTime: Date!
+    var dinnerTime: Date!
+    var focusTime: Float
+    var eatingTime: Float
+    var bedTime : Date!
+    var startTime: Date!
+    
+    init(){
+        focusTime = 0
+        eatingTime = 0
+        startTime =  Date()
+        lunchTime = Date()
+        dinnerTime = Date()
+        bedTime = Date()
+    }
+//    init() {
+//        let keychainItem : KeychainItemWrapper = KeychainItemWrapper(identifier: DataManager.keychainItemIdentifier, accessGroup: nil)
+//        focusTime = 0
+//        eatingTime = 0
+//        lunchTime = Date()
+//        dinnerTime = Date()
+//        startTime = Date()
+//        bedTime = Date()
+//       
+//            let superSecretValue = keychainItem[DataManager.dataManagerKey] as? String?
+//        if(superSecretValue! != nil){
+//            print("hey this works maybe")
+//            let array = self.convertJsonStringToDictionary(superSecretValue!!)
+//            for item in array!{
+//                self.createDataMangerFromDict(dict: item)
+//            }
+//        }
+//        
+//            
+//        
+//      
+//    }
+    
+    init?(startTime: Date, lunchTime: Date, dinnerTime: Date, bedTime: Date, focusTime: Float, eatingTime: Float) {
+        self.startTime = startTime
+        self.lunchTime = lunchTime
+        self.dinnerTime = dinnerTime
+        self.bedTime = bedTime
+        self.focusTime = focusTime
+        self.eatingTime = eatingTime
+    }
+
+    
+    //MARK: NSCoding
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(lunchTime, forKey: PropertyKey.lunchTimeKey)
+        aCoder.encode(dinnerTime, forKey: PropertyKey.dinnerTimeKey)
+        aCoder.encode(eatingTime, forKey: PropertyKey.eatingTimeKey)
+        aCoder.encode(focusTime, forKey: PropertyKey.focusTimeKey)
+        aCoder.encode(bedTime, forKey: PropertyKey.bedTimeKey)
+        aCoder.encode(startTime, forKey: PropertyKey.startTimeKey)
+
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        
+        // The name is required. If we cannot decode a name string, the initializer should fail.
+        guard let bedTimeDate = aDecoder.decodeObject(forKey: PropertyKey.bedTimeKey) as? Date else {
+            os_log("Unable to decode the name for a class object.", log: OSLog.default, type: .debug)
+            return nil
+        }
+        
+        guard let startTimeDate = aDecoder.decodeObject(forKey: PropertyKey.startTimeKey) as? Date else {
+            os_log("Unable to decode the name for a class object.", log: OSLog.default, type: .debug)
+            return nil
+        }
+        
+        guard let lunchTimeDate = aDecoder.decodeObject(forKey: PropertyKey.lunchTimeKey) as? Date else {
+            os_log("Unable to decode the name for a class object.", log: OSLog.default, type: .debug)
+            return nil
+        }
+        
+        guard let dinnerTimeDate = aDecoder.decodeObject(forKey: PropertyKey.dinnerTimeKey) as? Date else {
+            os_log("Unable to decode the name for a class object.", log: OSLog.default, type: .debug)
+            return nil
+        }
+        
+        let focusTimeFloat = aDecoder.decodeFloat(forKey: PropertyKey.focusTimeKey)
+        
+        let eatingTimeFloat = aDecoder.decodeFloat(forKey: PropertyKey.eatingTimeKey)
+        
+        self.init(startTime: startTimeDate, lunchTime: lunchTimeDate, dinnerTime: dinnerTimeDate, bedTime: bedTimeDate, focusTime: focusTimeFloat, eatingTime: eatingTimeFloat)
+        
+    }
+    
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("datamanager")
+    
+    
+    public func saveDataManager() {
+        let isSuccessfulSave = NSKeyedArchiver.archiveRootObject(DataManager.sharedInstance, toFile: DataManager.ArchiveURL.path)
+        if isSuccessfulSave {
+            os_log("Classes successfully saved.", log: OSLog.default, type: .debug)
+        } else {
+            os_log("Failed to save classes...", log: OSLog.default, type: .error)
+        }
+    }
+    
+    
+    private func loadDataManager() -> ()  {
+       DataManager.sharedInstance = (NSKeyedUnarchiver.unarchiveObject(withFile: DataManager.ArchiveURL.path) as? DataManager)!
+    }
+
+    
+ //   func toDict () -> Dictionary<String,AnyObject> {
+//        let dict: [String: AnyObject] = [DataManager.lunchTimeKey: lunchTime as AnyObject,
+//                                         DataManager.dinnerTimeKey: dinnerTime as AnyObject,
+//                                         DataManager.focusTimeKey: focusTime as AnyObject,
+//                                         DataManager.eatingTimeKey: eatingTime as AnyObject,
+//                                         DataManager.bedTimeKey: bedTime as AnyObject,
+//                                         DataManager.startTimeKey: startTime as AnyObject]
+//        return dict as Dictionary<String, AnyObject>    }
+//    
+//    func toString() -> NSString? {
+//        return convertDataToString(convertDictionaryToJsonData(toDict())!)
+//    }
+//    
+//    func convertDictionaryToJsonData(_ inputDict : Dictionary<String, AnyObject>) -> Data?{
+//        do{
+//            return try JSONSerialization.data(withJSONObject: inputDict, options:JSONSerialization.WritingOptions.prettyPrinted)
+//        }catch let error as NSError{
+//            print(error)
+//        }
+//        return nil
+//    }
+//    
+//    func convertDataToString(_ inputData : Data) -> NSString?{
+//        let returnString = String(data: inputData, encoding: String.Encoding.utf8)
+//        return returnString as NSString?
+//    }
+//
+//    func convertJsonStringToDictionary(_ text: String) -> Array<Dictionary<String, AnyObject>>? {if let data = text.data(using: String.Encoding.utf8) {
+//        do {
+//            return try JSONSerialization.jsonObject(with: data, options: []) as? Array<Dictionary<String, AnyObject>>
+//        } catch let error as NSError {
+//            print(error)
+//        }
+//        }
+//        return nil
+//    }
+//
+//    func secureKeyChain () -> (){
+//        let keychainItemWrapper = KeychainItemWrapper(identifier: DataManager.keychainItemIdentifier, accessGroup:nil)
+//        keychainItemWrapper[DataManager.dataManagerKey] = toString() as AnyObject?
+//    }
+//    
+//    func getKeyChain()->(){
+//        let keychainItemWrapper = KeychainItemWrapper(identifier: DataManager.keychainItemIdentifier, accessGroup: nil)
+//        let superSecretValue = keychainItemWrapper[DataManager.dataManagerKey] as? String?
+//        print("The super secret value is: \(String(describing: superSecretValue))");
+//        let array = convertJsonStringToDictionary(superSecretValue!!)
+//        for item in array!{
+//            createDataMangerFromDict(dict: item)
+//        }
+//        
+//    }
+//    
+//    func createDataMangerFromDict(dict: Dictionary<String, AnyObject>)->(){
+//        lunchTime = dict[DataManager.lunchTimeKey] as! Date
+//        dinnerTime = dict[DataManager.dinnerTimeKey] as! Date
+//        focusTime = dict[DataManager.focusTimeKey] as! Float
+//        eatingTime = dict[DataManager.eatingTimeKey] as! Float
+//        bedTime = dict[DataManager.bedTimeKey] as! Date
+//        startTime = dict[DataManager.startTimeKey] as! Date
+//    }
+
+}
--- TaskScheduler/SettingsViewController.swift
+++ TaskScheduler/SettingsViewController.swift
@@ -18,17 +18,50 @@ class SettingsViewController: UIViewController {
     
     @IBOutlet weak var focusTimePicker: UIDatePicker!
     @IBOutlet weak var eatingTimePicker: UIDatePicker!
+    
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBOutlet weak var revertButton: UIBarButtonItem!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
         // Do any additional setup after loading the view.
+        print("start load")
+        focusTimePicker.countDownDuration = TimeInterval(DataManager.sharedInstance.focusTime)
+        eatingTimePicker.countDownDuration = TimeInterval(DataManager.sharedInstance.eatingTime)
+        bedTimePicker.date = DataManager.sharedInstance.bedTime
+        lunchTimePicker.date = DataManager.sharedInstance.lunchTime
+        dinnerTimePicker.date = DataManager.sharedInstance.dinnerTime
+        startTimePicker.date = DataManager.sharedInstance.startTime
+        print("finish loading")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func revertAction(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func saveAction(_ sender: Any) {
+        DataManager.sharedInstance.bedTime = bedTimePicker.date
+        DataManager.sharedInstance.focusTime = Float(focusTimePicker.countDownDuration)
+        DataManager.sharedInstance.eatingTime = Float(eatingTimePicker.countDownDuration)
+        DataManager.sharedInstance.startTime = startTimePicker.date
+        DataManager.sharedInstance.lunchTime = lunchTimePicker.date
+        DataManager.sharedInstance.dinnerTime = dinnerTimePicker.date
+        
+        //DataManager.sharedInstance.secureKeyChain()
+        
+        print(DataManager.sharedInstance.bedTime)
+        print(DataManager.sharedInstance.startTime)
+        print(DataManager.sharedInstance.lunchTime)
+        print(DataManager.sharedInstance.dinnerTime)
+        print(DataManager.sharedInstance.focusTime)
+        print(DataManager.sharedInstance.eatingTime)
     }
     
 
